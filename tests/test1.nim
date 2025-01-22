4df5a57f83c7408eb82cfeffb3a0ffe7ad18e67d
xTrayambak <xtrayambak@disroot.org>
2025-01-22 15:46:19 +0530

    (add) tests: add test suite

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,11 +1,38 @@
+import std/unittest
 import ferrite/utf16view
 
-var view =
-  newUtf16View(@[0xD83C'u16, 0xDFF3'u16, 0xFE0F'u16, 0x26A7'u16], UTFEndianness.Host)
-echo view.codepointLen
-echo view.toUtf8
+var 
+  view =
+    newUtf16View(@[0xD83C'u16, 0xDFF3'u16, 0xFE0F'u16, 0x26A7'u16], UTFEndianness.Host)
 
-var view2 = newUtf16View("Hello! How are ya?")
+  view2 = newUtf16View("Hello! How are ya?")
 
-echo view2.codepointLen
-echo view2.toUtf8()
+  view3 =
+    newUtf16View(
+      @[0xD800'u16, 0x0041'u16], UTFEndianness.Host
+    )
+
+suite "UTF-16 View":
+  test "count codepoints (emoji)":
+    check view.codepointLen == 3
+
+  test "convert to UTF-8 (emoji)":
+    check view.toUtf8 == "🏳️"
+
+  test "start of view (emoji)":
+    check view.start == 0xD83C'u16
+
+  test "validation (emoji)":
+    check view.valid() == true
+
+  test "count codepoints (text)":
+    check view2.codepointLen == 18
+
+  test "convert to UTF-8 (text)":
+    check view2.toUtf8 == "Hello! How are ya?"
+
+  test "validation (text)":
+    check view2.valid() == true
+
+  test "validation (invalid UTF-16 sequence)":
+    check view3.valid() == false
--- tests/test2.nim
+++ tests/test2.nim
@@ -1,8 +1,30 @@
+import std/unittest
 import ferrite/utf8view
 
-var view = newUTF8View("Hello there!")
-let (valid, count) = view.validate()
+var 
+  view1 = newUTF8View("Hello there!")
+  view2 = newUTF8View(@[0x80'u8])
 
-echo valid
-echo count
-echo view.len
+suite "UTF-8 View":
+  test "count codepoints (text)":
+    check view1.len == 12
+
+  test "validate (text)":
+    check view1.valid() == true
+  
+  test "contains (text)":
+    check "Hello" in view1 == true
+    check "!" in view1 == true
+
+  test "to string (text)":
+    check view1.toString == "Hello there!"
+
+  test "count codepoints (malformed)":
+    check view2.len == 0
+
+  test "validate (malformed)":
+    check view2.valid == false
+
+  test "contains (malformed)":
+    check 0x80'u8 in view2
+    check 0x7F'u8 notin view2
